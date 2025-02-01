73b94af7755226d993610e7ddb3b86347f3d2df1
Yadim <v_kuhlmann@outlook.com>
2025-02-02 00:23:35 +0100

    fix

--- src/pages/Portfolioold/Portfolioold.jsx
+++ src/pages/Portfolioold/Portfolioold.jsx
@@ -1,20 +1,20 @@
-import './portfolio.scss';
+// import './portfolio.scss';
 
-import PortfolioCard from './PortfolioCard';
-import portfoliocardData from "./portfoliocardData";
+// import PortfolioCard from './PortfolioCard';
+// // import portfoliocardData from "./portfoliocardData";
 
-const Portfolio = () => {
+// const Portfolio = () => {
 
-    return (
-        <section className="portfolio section" id="">
-            <h2 className="visually-hidden">portfolio with my work</h2>
-            <div className="portfolio__container">
+//     return (
+//         <section className="portfolio section" id="">
+//             <h2 className="visually-hidden">portfolio with my work</h2>
+//             <div className="portfolio__container">
 
-                {portfoliocardData.map(item => <PortfolioCard item={item} key={item.id} />)}
+//                 {portfoliocardData.map(item => <PortfolioCard item={item} key={item.id} />)}
 
-            </div>
-        </section>
-    );
-}
+//             </div>
+//         </section>
+//     );
+// }
 
-export default Portfolio;
+// export default Portfolio;
